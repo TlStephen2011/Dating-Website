b9d6883cae23f6b0d105c7fbf1b2fcabd6f55ab5
tstephen <tstephen@student.wethinkcode.co.za>
2020-07-09 13:51:18 +0200

    added get all users with all details

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,9 +4,33 @@ const AuthService = require('../services/auth.service');
 const updateUserSchema = require('../validation/updateUser.schema');
 const { validationResult, checkSchema } = require("express-validator");
 
+function buildUserFields(users, req) {
+
+    let counter = 0;
+
+    return new Promise(async (resolve, reject) => {
+
+        for await (let user of users) {
+            const userImages = await req.services.imageService.getImages(user.id);
+            const userInterests = await req.services.userService.getInterests(user.id);
+            user.images = userImages.images;
+            user.interests = userInterests.interests;
+            counter++;
+        }
+
+        if (counter === users.length)
+            resolve(users);
+        else
+            reject('Not looped enough');
+    })
+}
+
+
 router.get('/all', AuthService.checkAuth, async (req, res) => {
     try {
         const users = await req.services.userService.getAllUsers();
+
+        const joinedUsers = await buildUserFields(users.users, req);
 
         const filteredUsers = users.users.map(x => {
             return {
@@ -22,11 +46,16 @@ router.get('/all', AuthService.checkAuth, async (req, res) => {
                 lastOnline: x.lastOnline,
                 interests: x.interests,
                 dateOfBirth: x.dateOfBirth,
-                fameRating: x.fameRating
+                fameRating: x.fameRating,
+                images: x.images,
+                interests: x.interests
             }
         })
 
-        res.json(filteredUsers);
+        res.json({
+            success: true,
+            users: filteredUsers
+        });
     } catch (error) {
         res.json(error)
     }
